525291c5f7e180cd4b38f65b7701c75a0e865a27
guptakaran11 <gupta.karan1162@gmail.com>
2024-02-29 22:17:48 +0530

    screen ui start

---
+++ lib/models/moviesList.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:entertainment/models/appConfigModel.dart';
+import 'package:get_it/get_it.dart';
+
+class MoviesList {
+  final String name;
+  final String language;
+  final bool isAdult;
+  final String description;
+  final String posterPath;
+  final String backDropPath;
+  final num rating;
+  final String releaseDate;
+
+  MoviesList({
+    required this.name,
+    required this.language,
+    required this.isAdult,
+    required this.description,
+    required this.posterPath,
+    required this.backDropPath,
+    required this.rating,
+    required this.releaseDate,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': name,
+      'original_language': language,
+      'adult': isAdult,
+      'overview': description,
+      'poster_path': posterPath,
+      'backdrop_path': backDropPath,
+      'vote_average': rating,
+      'release_date': releaseDate,
+    };
+  }
+
+  factory MoviesList.fromMap(Map<String, dynamic> map) {
+    return MoviesList(
+      name: map['title'] as String,
+      language: map['original_language'] as String,
+      isAdult: map['adult'] as bool,
+      description: map['overview'] as String,
+      posterPath: map['poster_path'] as String,
+      backDropPath: map['backdrop_path'] as String,
+      rating: map['vote_average'] as num,
+      releaseDate: map['release_date'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MoviesList.fromJson(String source) =>
+      MoviesList.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  String posterURL() {
+    final AppConfigModel appConfigModel = GetIt.instance.get<AppConfigModel>();
+    return '${appConfigModel.BASE_IMAGE_API_URL}$posterPath';
+  }
+}
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore_for_file: must_be_immutable
 
 import 'dart:ui';
 
+import 'package:entertainment/models/moviesList.dart';
+import 'package:entertainment/models/searchCategory.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,24 +12,33 @@ class HomePage extends ConsumerWidget {
   double? height;
   double? width;
 
-  HomePage({super.key});
+  TextEditingController? searchTextFieldcontroller;
+  HomePage({
+    super.key,
+    this.height,
+    this.width,
+    this.searchTextFieldcontroller,
+  });
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
+    searchTextFieldcontroller = TextEditingController();
     return buildUI();
   }
 
   Widget buildUI() {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
+      body: SizedBox(
         height: height,
         width: width,
         child: Stack(
           alignment: Alignment.center,
           children: [
             backgrouondWidget(),
+            foregroundWidgets(),
           ],
         ),
       ),
@@ -62,5 +74,161 @@ class HomePage extends ConsumerWidget {
         ),
       ),
     );
+  }
+
+  Widget foregroundWidgets() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, height! * 0.02, 0, 0),
+      width: height! * 0.88,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          topBarWidget(),
+          Container(
+            height: height! * 0.83,
+            padding: EdgeInsets.symmetric(
+              vertical: height! * 0.01,
+            ),
+            child: movieListViewWidget(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget topBarWidget() {
+    return Container(
+      height: height! * 0.08,
+      decoration: BoxDecoration(
+        color: Colors.black54,
+        borderRadius: BorderRadius.circular(
+          20.0,
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          searchFieldWidget(),
+          categorySelectionWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget searchFieldWidget() {
+    const border = InputBorder.none;
+    return SizedBox(
+      width: width! * 0.50,
+      height: height! * 0.05,
+      child: TextField(
+        controller: searchTextFieldcontroller,
+        onSubmitted: (input) {},
+        style: const TextStyle(color: Colors.white),
+        decoration: const InputDecoration(
+          focusedBorder: border,
+          border: border,
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.white24,
+          ),
+          hintStyle: TextStyle(
+            color: Colors.white54,
+          ),
+          filled: false,
+          fillColor: Colors.white24,
+          hintText: 'Search Movie....',
+        ),
+      ),
+    );
+  }
+
+  Widget categorySelectionWidget() {
+    return DropdownButton(
+      dropdownColor: Colors.black38,
+      value: SearchCategory.popular,
+      icon: const Icon(
+        Icons.menu,
+        color: Colors.white24,
+      ),
+      underline: Container(
+        height: 1,
+        color: Colors.white24,
+      ),
+      onChanged: (value) {},
+      items: const [
+        DropdownMenuItem(
+          value: SearchCategory.popular,
+          child: Text(
+            SearchCategory.popular,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        DropdownMenuItem(
+          value: SearchCategory.upcoming,
+          child: Text(
+            SearchCategory.upcoming,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        DropdownMenuItem(
+          value: SearchCategory.none,
+          child: Text(
+            SearchCategory.none,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget movieListViewWidget() {
+    final List<MoviesList> movies = [];
+
+    for (var i = 0; i < 20; i++) {
+      movies.add(
+        MoviesList(
+          name: "Mortal Kombat",
+          language: "EN",
+          isAdult: false,
+          description: "A fantastic game",
+          posterPath: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+          backDropPath: "/hZkgoQYus5vegHoetLkCJzb17zJ.jpg",
+          rating: 7.8,
+          releaseDate: "07-01-2024",
+        ),
+      );
+    }
+
+    if (movies.isNotEmpty) {
+      return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: height! * 0.01,
+              ),
+              child: GestureDetector(
+                onTap: () {},
+                child: Text(movies[index].name),
+              ),
+            );
+          });
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.white,
+        ),
+      );
+    }
   }
 }
