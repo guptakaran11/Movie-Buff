4fd3f0c4364999e55498fe196f171c2b2c303bcd
guptakaran11 <gupta.karan1162@gmail.com>
2024-03-10 23:15:28 +0530

    small changes

--- lib/models/homePageModel.dart
+++ lib/models/homePageModel.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: file_names
-
 import 'package:entertainment/models/moviesList.dart';
 import 'package:entertainment/models/searchCategory.dart';
 
--- lib/models/searchCategory.dart
+++ lib/models/searchCategory.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: file_names
-
 class SearchCategory {
   static const String popular = 'Popular';
   static const String upcoming = 'Upcoming';
